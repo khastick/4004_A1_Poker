5d48b787de22f2b3d4aa0ea9b64e44d793d109c7
khastick <h_khama5@msn.com>
2015-09-27 21:34:29 -0400

    Added test and functions to add cards through a string input.

---
+++ 4004_A1_Poker/src/poker/Player.java
@@ -0,0 +1,76 @@
+package poker;
+
+import java.util.Arrays;
+
+public class Player {
+	static String[] SUIT = {"Hearts", "Spades", "Diamonds", "Clubs" };
+	static String[] RANK = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", 
+			"Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace" };
+	static int SUIT_SIZE = 14;
+	
+	int[] cards;
+	int id, chips;
+	
+	public int[] getCards(){
+		return cards;
+	}
+	
+	public Player(int aId, int aChips, int[] aCards){
+		id = aId;
+		chips = aChips;
+		cards = aCards;
+	}
+	
+	public Player(String aHand){
+		id = 0;
+		chips = 0;
+		cards = new int[5];
+		StringToHand(aHand);
+	}
+	
+	public int[] Check(){
+		return new int[]{0,0};
+	}
+	
+	public int[] Call(){
+		return new int[]{1,0};
+	}
+	
+	public int[] Raise(){
+		return new int[]{2,0};
+	}
+	
+	public int[] Open(){
+		return new int[]{3,0};
+	}
+	
+	public int[] Discard(){
+		return new int[]{};
+	}
+	
+	public String HandToString(){
+		String hand = "";
+		for(int i = 0; i < cards.length; i++){
+			int rankIndex = (int)cards[i] % SUIT_SIZE;
+			int suitIndex = (int)Math.floor(cards[i]/SUIT_SIZE); 
+			hand += RANK[rankIndex] + SUIT[suitIndex] + " ";
+		}
+		return hand.trim();
+	}
+	
+	public void StringToHand(String aHand){
+		String[] handSplit = aHand.split(" ");
+		String[] cardSplit;
+		
+		for(int i = 0; i < cards.length; i++){
+			cardSplit = handSplit[i].split("(?=\\p{Upper})");
+			
+			int groupValue = Arrays.asList(RANK).indexOf(cardSplit[0]) ;
+			int typeValue = Arrays.asList(SUIT).indexOf(cardSplit[1]) * SUIT_SIZE;
+			
+			cards[i] = groupValue + typeValue;
+		}
+		
+	}
+
+}
--- 4004_A1_Poker/src/test/PokerTest.java
+++ 4004_A1_Poker/src/test/PokerTest.java
@@ -2,13 +2,33 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
+
+import poker.Player;
 
 public class PokerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStringToHand(){
+		Player player = new Player("OneHearts TwoHearts AceSpades KingDiamonds EightClubs");		
+		int [] playerCards, someCards;
+		
+		playerCards = player.getCards();
+		someCards = new int[]{0,1,27,40,49};
+		
+		assertTrue(Arrays.equals(playerCards, someCards));
 	}
-
+	
+	@Test
+	public void testHandToString(){
+		String inputHand = "OneHearts TwoHearts AceSpades KingDiamonds EightClubs";
+		String testHand;
+		Player player = new Player(inputHand);		
+		
+		testHand = player.HandToString();
+		
+		assertTrue(inputHand.equals(testHand));
+	}
 }
